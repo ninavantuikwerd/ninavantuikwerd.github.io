bbd28372381f35e46a0232d3f3c9cad87c7d6235
ninavantuikwerd <37994090+ninavantuikwerd@users.noreply.github.com>
2018-04-05 17:27:27 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+// var $grid = $('.grid').masonry({
+//   // set itemSelector so .grid-sizer is not used in layout
+//   itemSelector: '.grid-item',
+//   // use element for option
+//   columnWidth: 200,
+//   gutter: '.gutter-sizer',
+//   percentPosition: true
+// });
+//
+//
+// // layout Masonry after each image loads
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry('layout');
+// });
+
+
+// $(document).ready(function()
+// {
+//     $("#imgAnimate").hover(
+//         function()
+//         {
+//             $(this).attr("src", "panelen.gif");
+//         },
+//         function()
+//         {
+//             $(this).attr("src", "assets/panelen/panelen5.jpg");
+//         });
+// });
